fbd0cb657e035ba3d6f2be311de690d1900a4a89
Mari Gonzalez <karima19@gmail.com>
2016-03-09 21:03:04 -0800

    made for and while loops of students

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//window.alert("Hi!");
+var students = ["Ilya", "George", "Allie", "Ahmed", "Singh"];
+var index = 0;
+for (var index = 0; index < 5; index++) {
+  console.log(students[index]);
+}
+
+
+
+var students2 = ["Jenny", "Evan", "Matt", "Lisa", "Christina"]
+var i = 0;
+while (i < 5) {
+  console.log(students2[i])
+  i++;
+}
